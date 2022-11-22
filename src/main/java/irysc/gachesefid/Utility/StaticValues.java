f47b809c042791bcfe58539217e6b177669ba6a9
mghaneh1375 <mghaneh1375>
2022-11-22 20:00:44 +0330

    nothing

--- src/main/java/irysc/gachesefid/Utility/StaticValues.java
+++ src/main/java/irysc/gachesefid/Utility/StaticValues.java
@@ -46,7 +46,7 @@ public class StaticValues {
     public final static int MAX_FILE_SIZE = ONE_MB * 6;
 
     public final static boolean LOCAL = true;
-    public final static boolean DEV_MODE = false;
+    public final static boolean DEV_MODE = true;
 
 
     public static final String ANSI_RESET = "\u001B[0m";
