283268763adf0bb3484753fcef4e1060eee0f70d
mohammad ghane <mghaneh1375@yahoo.com>
2023-02-11 11:35:21 +0330

    nothing

--- src/main/java/irysc/gachesefid/Utility/StaticValues.java
+++ src/main/java/irysc/gachesefid/Utility/StaticValues.java
@@ -7,9 +7,9 @@ import org.json.JSONObject;
 
 public class StaticValues {
 
-//    public final static String STATICS_SERVER = "https://statics.irysc.com/";
+    public final static String STATICS_SERVER = "https://statics.irysc.com/";
 
-    public final static String STATICS_SERVER = "http://localstaticgach.com/";
+//    public final static String STATICS_SERVER = "http://localstaticgach.com/";
 //    public final static String GACH_SERVER = "http://192.168.43.157:8080/api/";
 
 
@@ -49,7 +49,7 @@ public class StaticValues {
     public final static int MAX_FILE_SIZE = ONE_MB * 6;
 
     public final static boolean LOCAL = true;
-    public final static boolean DEV_MODE = true;
+    public final static boolean DEV_MODE = false;
 
 
     public static final String ANSI_RESET = "\u001B[0m";
