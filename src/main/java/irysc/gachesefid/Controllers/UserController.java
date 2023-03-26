2fd1031b0fc83027f2948442059e225e27696c3b
mohammad ghane <mghaneh1375@yahoo.com>
2023-03-26 18:41:46 +0430

    nothing

--- src/main/java/irysc/gachesefid/Controllers/UserController.java
+++ src/main/java/irysc/gachesefid/Controllers/UserController.java
@@ -1052,13 +1052,12 @@ public class UserController {
         Document config = getConfig();
 
         double exchangeRate = ((Number)config.get("coin_rate_coef")).doubleValue();
-        double moneyRate = Math.round((10000.0 / exchangeRate) * 100.0) / 100.0;
 
         JSONObject jsonObject = new JSONObject()
                 .put("money", user.get("money"))
                 .put("coinToMoneyExchange", exchangeRate)
 //                .put("moneyToCoinExchange", config.get("money_rate_coef"))
-                .put("moneyToCoinExchange", moneyRate)
+                .put("moneyToCoinExchange", String.format("%.2f", 10000.0 / exchangeRate))
                 .put("rank", rank == null ? "" : rank.getInteger("rank"))
                 .put("branchRank", 1)
                 .put("gradeRank", rank == null || !rank.containsKey("grade_rank") ? "" : rank.getInteger("grade_rank"))
