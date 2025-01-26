4b5b20c0961851b30e0b79ac6ef32bedbfec3b78
mghaneh1375 <mghaneh1375@gmail.com>
2025-01-26 20:42:32 +0330

    Don't return rank in getKarname.
    (cherry picked from commit 2055cf8f1767cd0c7df23ab237cba9980455d357)

--- src/main/java/irysc/gachesefid/Utility/Utility.java
+++ src/main/java/irysc/gachesefid/Utility/Utility.java
@@ -1329,6 +1329,8 @@ public class Utility {
 
         if (!jsonObject1.has("branches"))
             jsonObject1.put("branches", "");
+
+        jsonObject1.put("rank", -1);
     }
     public static void fillJSONWithUser(JSONObject jsonObject, Document user) {
 
