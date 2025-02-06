1844dd81141ee176773d9aa7cd141a72c97d2b9c
mghaneh1375 <mghaneh1375@gmail.com>
2025-02-06 17:56:40 +0330

    Remove redundant rss items.
    (cherry picked from commit cec1436e5b4d6088ca8b1d42715603f19712865b)

--- src/main/java/irysc/gachesefid/Controllers/Jobs.java
+++ src/main/java/irysc/gachesefid/Controllers/Jobs.java
@@ -31,7 +31,7 @@ import static irysc.gachesefid.Main.GachesefidApplication.*;
 import static irysc.gachesefid.Security.JwtTokenFilter.blackListTokens;
 import static irysc.gachesefid.Utility.SkyRoomUtils.deleteMeeting;
 import static irysc.gachesefid.Utility.StaticValues.*;
-import static irysc.gachesefid.Utility.Utility.sendSMSWithTemplate;
+import static irysc.gachesefid.Utility.Utility.*;
 
 public class Jobs implements Runnable {
 
@@ -370,6 +370,13 @@ public class Jobs implements Runnable {
         }
     }
 
+    private static class RejectExpiredRSS extends TimerTask {
+        @Override
+        public void run() {
+            int today = getPast(5);
+            rssRepository.deleteMany(lt("today", today));
+        }
+    }
     private static class RejectExpiredTeachRequests extends TimerTask {
         @Override
         public void run() {
