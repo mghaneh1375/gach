48d80f441e25c134a73a6496ce3f305e5ea5e0a3
mghaneh1375 <mghaneh1375@gmail.com>
2025-03-12 20:54:13 +0330

    Change sms phone number.

--- src/main/java/irysc/gachesefid/Utility/Utility.java
+++ src/main/java/irysc/gachesefid/Utility/Utility.java
@@ -368,7 +368,7 @@ public class Utility {
             HttpResponse<String> response = Unirest.post("https://panel.asanak.com/webservice/v1rest/sendsms")
                     .queryString("username", "bogenGach") // bogenGach
                     .queryString("password", "9DGr7JwEUXLtyVee")
-                    .queryString("source", "982100013860625")
+                    .queryString("source", "9821700013860625")
                     .queryString("destination", receptor)
                     .queryString("send_to_blacklist", 1)
                     .queryString("message", msg)
