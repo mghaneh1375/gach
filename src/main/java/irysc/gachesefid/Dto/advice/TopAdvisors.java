a0206b989cf8cd10d356585b70d4272f054eecaa
mghaneh1375 <mghaneh1375@gmail.com>
2025-10-10 20:54:50 +0330

    Admin panel in progress.

--- src/main/java/irysc/gachesefid/Dto/advice/TopAdvisors.java
+++ src/main/java/irysc/gachesefid/Dto/advice/TopAdvisors.java
@@ -1,10 +1,13 @@
 package irysc.gachesefid.Dto.advice;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class TopAdvisors extends AdvisorDigestInfoDto {}
