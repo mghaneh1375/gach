8bff13c7cc5af583c8940173229b304e83a7a564
mghaneh1375 <mghaneh1375@gmail.com>
2025-10-31 17:15:20 +0330

    Fix bug.

--- src/main/java/irysc/gachesefid/Security/SecurityConfig.java
+++ src/main/java/irysc/gachesefid/Security/SecurityConfig.java
@@ -124,6 +124,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/api/quiz/manage/removeCorrectors/**",
                         "/api/quiz/manage/addCorrector/**",
                         "/api/quiz/manage/getCorrector/**",
+                        "/api/quiz/manage/getDistinctTags",
                         "/api/quiz/manage/getCorrectors/**",
                         "/api/quiz/manage/generateQuestionPDF/**",
                         "/api/quiz/manage/addBatchQuestionsToQuiz/**",
